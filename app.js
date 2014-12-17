a874db4fa6de72928232e80600f2bdc56ef7c895
James <robinson039@gmail.com>
2014-12-17 11:17:00 -0500

    added scripts

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 
 /**
-	* Node.js Login Boilerplate
-	* More Info : http://bit.ly/LsODY8
-	* Copyright (c) 2013 Stephen Braitsch
+	Login App
 **/
 
 var express = require('express');
 var http = require('http');
 var app = express();
 
-app.configure(function(){
-	app.set('port', 80);
+/*
+app.get('/', function (req, res) {
+  res.send('hello world');
+	//res.send('MetLife Tests #3');
+	//comment ewr
+}); */
+
+app.configure(function() {
+	//res.send('hello world');
+        app.set('port', 80);
 	app.set('views', __dirname + '/app/server/views');
 	app.set('view engine', 'jade');
 	app.locals.pretty = true;
@@ -22,14 +28,28 @@ app.configure(function(){
 	app.use(express.methodOverride());
 	app.use(require('stylus').middleware({ src: __dirname + '/app/public' }));
 	app.use(express.static(__dirname + '/app/public'));
+//        app.send(port);
 });
+
+
 
 app.configure('development', function(){
 	app.use(express.errorHandler());
 });
 
+
 require('./app/server/router')(app);
 
 http.createServer(app).listen(app.get('port'), function(){
 	console.log("Express server listening on port " + app.get('port'));
+
+app.get('/', function (req, res) {
+  res.send('hello world');
+        //res.send('MetLife Tests #3');
+        //comment ewr
+});
+
+
+//});
 })
+
