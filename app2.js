8203b688b901d04fbababc910e9781b7097108e4
jay <robinson039@gmail.com>
2014-12-17 21:31:59 -0500

    error test

--- app2.js
+++ app2.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('hello world');
+  res.send('hello worlda');
         //res.send('MetLife Tests #3');
         //comment ewr
 });
